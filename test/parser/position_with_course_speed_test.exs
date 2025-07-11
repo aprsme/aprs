4ce70de75f52db2f1d54e81dbc8d2f612c8091a9
Graham McIntire <graham@mcintire.me>
2025-07-11 14:25:47 -0500

    more mice test

--- test/parser/position_with_course_speed_test.exs
+++ test/parser/position_with_course_speed_test.exs
@@ -4,25 +4,26 @@ defmodule Aprs.Parser.PositionWithCourseSpeedTest do
   describe "position packets with course/speed" do
     test "KG5GKC-12 position packet with course/speed should be parsed as position" do
       # This packet was being misclassified as weather
-      packet = "KG5GKC-12>APAT51,WIDE1-1,WIDE2-2,qAO,NI2C-10:!3310.04N/09640.40Wk038/023/A=000623AT-MOBILE KG5GKC@YAHOO.COM"
-      
+      packet =
+        "KG5GKC-12>APAT51,WIDE1-1,WIDE2-2,qAO,NI2C-10:!3310.04N/09640.40Wk038/023/A=000623AT-MOBILE KG5GKC@YAHOO.COM"
+
       {:ok, parsed} = Aprs.parse(packet)
-      
+
       # Should be identified as position
       assert parsed.data_type == :position
       assert parsed.sender == "KG5GKC-12"
       assert parsed.destination == "APAT51"
-      
+
       # Check position data
       assert parsed.data_extended[:latitude]
       assert parsed.data_extended[:longitude]
       assert parsed.data_extended[:symbol_code] == "k"
       assert parsed.data_extended[:symbol_table_id] == "/"
-      
+
       # Check course/speed extraction
       assert parsed.data_extended[:course] == 38
       assert parsed.data_extended[:speed] == 23.0
-      
+
       # Should not have weather fields
       refute parsed.data_extended[:temperature]
       refute parsed.data_extended[:humidity]
@@ -34,9 +35,9 @@ defmodule Aprs.Parser.PositionWithCourseSpeedTest do
     test "position packet with different symbol should not be weather" do
       # Car symbol (>) with course/speed
       packet = "N0CALL>APRS:!3310.04N/09640.40W>123/045/A=001000"
-      
+
       {:ok, parsed} = Aprs.parse(packet)
-      
+
       assert parsed.data_type == :position
       assert parsed.data_extended[:symbol_code] == ">"
       assert parsed.data_extended[:symbol_table_id] == "/"
@@ -46,9 +47,9 @@ defmodule Aprs.Parser.PositionWithCourseSpeedTest do
 
     test "position with PHG data should not be weather" do
       packet = "N0CALL>APRS:!3310.04N/09640.40W#PHG5130/W3,FLn comment"
-      
+
       {:ok, parsed} = Aprs.parse(packet)
-      
+
       assert parsed.data_type == :position
       assert parsed.data_extended[:symbol_code] == "#"
       assert parsed.data_extended[:comment] =~ "PHG5130"
@@ -57,9 +58,9 @@ defmodule Aprs.Parser.PositionWithCourseSpeedTest do
     test "weather station symbol without weather data is just position" do
       # Weather symbol "_" but no weather data
       packet = "N0CALL>APRS:!3310.04N/09640.40W_No weather data here"
-      
+
       {:ok, parsed} = Aprs.parse(packet)
-      
+
       # Parser correctly identifies as position since no weather data
       assert parsed.data_type == :position
       assert parsed.data_extended[:symbol_code] == "_"
@@ -69,9 +70,9 @@ defmodule Aprs.Parser.PositionWithCourseSpeedTest do
     test "actual weather packet should be identified as weather" do
       # Real weather format with "_" prefix and weather data
       packet = "KC0ABC>APRS:_10090556c220/004g005t077r000p000P000h50b09900"
-      
+
       {:ok, parsed} = Aprs.parse(packet)
-      
+
       assert parsed.data_type == :weather
       assert parsed.data_extended[:temperature] == 77
       assert parsed.data_extended[:humidity] == 50
@@ -85,21 +86,21 @@ defmodule Aprs.Parser.PositionWithCourseSpeedTest do
       # Position packet with weather station symbol and weather data patterns
       # This is a position packet that also reports weather
       packet = "N0CALL>APRS:!3310.04N/09640.40W_220/004g005t077r000p000P000h50b09900"
-      
+
       {:ok, parsed} = Aprs.parse(packet)
-      
+
       # Parser identifies as position (because of ! prefix)
       # but also extracts weather data from comment
       assert parsed.data_type == :position
       assert parsed.data_extended[:symbol_code] == "_"
-      
+
       # Weather data is extracted
       assert parsed.data_extended[:temperature] == 77
       assert parsed.data_extended[:humidity] == 50
       assert parsed.data_extended[:pressure] == 990.0
       assert parsed.data_extended[:wind_direction] == 220
       assert parsed.data_extended[:wind_speed] == 4
-      
+
       # Also has position data
       assert parsed.data_extended[:latitude]
       assert parsed.data_extended[:longitude]
